4a393eb3fe2e7e0ddec4e27da3f6760265f6a9d2
SJ KIM <sk1087sk@gmail.com>
2024-03-05 22:00:02 +0100

    typeprops

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types'
+import DefaultPicture from '../../assets/profile.png'
 
 function Card({ label, title, picture }) {
   return (
-    <div>
-      <span style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-        {label}
-      </span>
+    <div  style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+      <span>{label}</span>
       <img src={picture} alt="freelance" height={80} width={80} />
       <span>{title}</span>
     </div>
@@ -13,9 +12,15 @@ function Card({ label, title, picture }) {
 }
 
 Card.propTypes = {
-  label: PropTypes.string,
-  title: PropTypes.string,
-  picture: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+}
+
+Card.defaultProps = {
+  label: "",
+  title: "",
+  picture: DefaultPicture,
 }
 
 export default Card
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,21 +1,17 @@
-import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card'
 
 const freelanceProfiles = [
   {
     name: 'Jane Doe',
     jobTitle: 'Devops',
-    picture: DefaultPicture,
   },
   {
     name: 'John Doe',
     jobTitle: 'Developpeur frontend',
-    picture: DefaultPicture,
   },
   {
     name: 'Jeanne Biche',
     jobTitle: 'Développeuse Fullstack',
-    picture: DefaultPicture,
   },
 ]
 
@@ -27,7 +23,6 @@ function Freelances() {
         <Card
           key={`${profile.name}-${index}`}
           label={profile.jobTitle}
-          picture={profile.picture}
           title={profile.name}
         />
       ))}
